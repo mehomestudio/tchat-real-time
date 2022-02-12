4ea798f1c883a5befc93269d77b176cf5181f37b
Styx <mehdih.devapp@gmail.com>
2022-02-12 11:56:26 +0100

    Start project

---
+++ docker/Dockerfile.php
@@ -0,0 +1,14 @@
+FROM php:latest
+RUN mv "$PHP_INI_DIR/php.ini-development" "$PHP_INI_DIR/php.ini"
+RUN curl -sS https://getcomposer.org/installer | php -- --filename=composer --install-dir=/usr/local/bin
+RUN curl -sS https://get.symfony.com/cli/installer | bash
+RUN apt update && apt install -y zip git libicu-dev locales
+RUN docker-php-ext-install opcache intl pdo_mysql
+RUN mv /root/.symfony/bin/symfony /usr/local/bin/symfony
+RUN locale-gen fr_FR.UTF-8
+WORKDIR /app
+COPY ./app/composer.json .
+RUN composer install
+COPY ./app .
+RUN symfony server:ca:install
+CMD ["symfony", "serve"]
