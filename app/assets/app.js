1ade796d74b07f02ed75be4bd040836cb9fa81ce
Styx <mehdih.devapp@gmail.com>
2022-02-12 16:48:34 +0100

    feat: add authentification

--- app/assets/app.js
+++ app/assets/app.js
@@ -7,3 +7,5 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+
+import './scripts/app';
--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
+    #[isGranted('IS_AUTHENTICATED_FULLY')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
         ]);
     }
 }
