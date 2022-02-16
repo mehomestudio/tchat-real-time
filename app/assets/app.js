140fd43a6cf7a1ef1fa9161ef8bf9e42d83f0052
Styx <mehdih.devapp@gmail.com>
2022-02-17 00:24:54 +0100

    fix: docker with --no-script for Dockerfile.php

--- app/assets/app.js
+++ app/assets/app.js
@@ -7,5 +7,3 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
-
-import './scripts/app';
--- app/config/bundles.php
+++ app/config/bundles.php
@@ -7,10 +7,10 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
