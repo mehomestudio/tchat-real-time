0b18f866615f1c116fa34c13bc72c218b7516c2d
Styx <mehdih.devapp@gmail.com>
2022-02-17 00:28:03 +0100

    feat: add feature validation email and checking

--- app/src/Controller/SecurityController.php
+++ app/src/Controller/SecurityController.php
@@ -2,8 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
+use App\Function\TokenFunc;
+use App\Repository\UserRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -23,7 +34,113 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+
+    #[Route('/inscription', name: 'app_register')]
+    public function register(
+        Request $request,
+        ManagerRegistry $manager,
+        UserPasswordHasherInterface $hasher,
+        MailerInterface $mailer
+    ): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
+
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $token = TokenFunc::generateToken();
+            $user
+                ->setPassword($hasher->hashPassword($user, $user->getPassword()))
+                ->setTokenEmail(sha1($token));
+            $manager->getManager()->persist($user);
+            $manager->getManager()->flush();
+
+            $email = (new TemplatedEmail())
+                ->to($user->getEmail())
+                ->subject('Validation de votre adresse email')
+                ->htmlTemplate('emails/templates/register.html.twig')
+                ->context([
+                    'pseudo' => $user->getPseudo(),
+                    'token' => $token
+                ]);
+
+            try {
+                $mailer->send($email);
+            } catch (TransportExceptionInterface $e) {
+            }
+
+            $this->addFlash('notice', 'Inscription réussie, vous avez reçu un email pour vérification.');
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/register.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/email/verified/{pseudo}/{token}', name: 'app_email_verified')]
+    #[Entity('user', options: ['mapping' => ['pseudo' => 'pseudo']])]
+    public function verifiedEmail(
+        ?User $user,
+        string $token,
+        ManagerRegistry $manager,
+        MailerInterface $mailer
+    ): Response
+    {
+        $notice = 'Le token est incorrect ou expiré, vous allez recevoir un email avec un nouveau lien.';
+
+        if ($user) {
+            $htmlTemplate = "email-verified-success.html.twig";
+            $subject = 'Validation de votre adresse email';
+            $context = [
+                'pseudo' => $user->getPseudo()
+            ];
+
+            if ($user->getTokenEmail() === sha1($token)) {
+                $user
+                    ->setTokenEmail(null)
+                    ->setIsEmailVerified(true);
+
+                $notice = "Votre email a été vérifié avec succès !";
+            } else {
+                $token = TokenFunc::generateToken();
+                $user->setTokenEmail(sha1($token));
+
+                $subject = "Vérification de l'email échouée.";
+                $htmlTemplate = "email-verified-failed.html.twig";
+                $context['token'] = $token;
+
+            }
+            $manager->getManager()->persist($user);
+            $manager->getManager()->flush();
+
+            $email = (new TemplatedEmail())
+                ->to($user->getEmail())
+                ->subject($subject)
+                ->htmlTemplate('emails/templates/'.$htmlTemplate)
+                ->context($context);
+
+            try {
+                $mailer->send($email);
+            } catch (TransportExceptionInterface $e) {
+            }
+
+        }
+
+        $this->addFlash('notice', $notice);
+
+        return $this->redirectToRoute('app_login');
     }
 
     /**
--- app/src/Security/SecurityAuthenticator.php
+++ app/src/Security/SecurityAuthenticator.php
@@ -2,11 +2,13 @@
 
 namespace App\Security;
 
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
@@ -21,11 +23,12 @@ class SecurityAuthenticator extends AbstractLoginFormAuthenticator
 
     public const LOGIN_ROUTE = 'app_login';
 
-    private UrlGeneratorInterface $urlGenerator;
 
-    public function __construct(UrlGeneratorInterface $urlGenerator)
+
+    public function __construct(
+        private UrlGeneratorInterface $urlGenerator
+    )
     {
-        $this->urlGenerator = $urlGenerator;
     }
 
     public function authenticate(Request $request): Passport
